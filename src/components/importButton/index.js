997d9ecb3760382dfab6159254fc05c4c4fc3b08
Julien Matondo <julien.matondo-tezolo@bothive.be>
2022-02-07 17:18:18 +0100

    removing multiple useState()

--- src/components/importButton/index.js
+++ src/components/importButton/index.js
@@ -4,35 +4,32 @@ import importImage from "../../../public/images/import_120260.svg";
 import styles from "./style.module.scss";
 
 const ImportButton = () => {
-  const [fileName, setFileName] = useState("");
-  const [file, setFile] = useState("");
+  const [file, setFile] = useState({});
 
-  const getFile = (e) => {
-    setFileName(e.target.files[0].name);
-
+  const getFileData = (e) => {
     let file = e.target.files[0];
     let reader = new FileReader();
 
     reader.onload = function (event) {
-      setFile(event.target.result);
+      setFile({ name: e.target.files[0].name, content: event.target.result });
     };
 
     reader.readAsText(file);
   };
 
   return (
-    <div className={styles.buttonContainer}>
+    <div>
       <div className={styles.button}>
         <Image src={importImage} alt="Import file" width={20} height={20} />
         <p>Import file</p>
-        <input type="file" accept=".html" onChange={(e) => getFile(e)}></input>
+        <input type="file" accept=".html" onChange={(e) => getFileData(e)}></input>
       </div>
 
       <p>
-        Document name: <strong>{fileName}</strong>
+        Document name: <strong>{file.name}</strong>
       </p>
 
-      <p>{file}</p>
+      <p>{file.content}</p>
     </div>
   );
 };
