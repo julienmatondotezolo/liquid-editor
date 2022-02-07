7f088e1f0d5c3bb2ea87cce561823fc601a0b76d
Julien Matondo <julien.matondo-tezolo@bothive.be>
2022-02-07 16:30:55 +0100

    styling button

--- src/components/importButton/index.js
+++ src/components/importButton/index.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Image from "next/image";
+import importImage from "../../../public/images/import_120260.svg";
+import styles from "./style.module.scss";
 
 const ImportButton = () => {
-  return <p>import file</p>;
+  return (
+    <div className={styles.buttonContainer}>
+      <div className={styles.button}>
+        <Image src={importImage} alt="Import file" width={20} height={20} />
+        <p>Import file</p>
+        <input type="file" accept=".html"></input>
+      </div>
+    </div>
+  );
 };
 
 export default ImportButton;
