75001aa2ed9bc622268d09ee0daf4ef38dddd2cb
Julien Matondo <julien.matondo-tezolo@bothive.be>
2022-02-22 10:43:57 +0100

    changing alert time

--- pages/_app.js
+++ pages/_app.js
@@ -3,19 +3,15 @@ import "../src/assets/styles/globals.scss";
 import { positions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
 
-import Layout from "../src/components/layout";
-
 const options = {
-  timeout: 5000,
+  timeout: 3000,
   position: positions.BOTTOM_CENTER,
 };
 
 function MyApp({ Component, pageProps }) {
   return (
     <AlertProvider template={AlertTemplate} {...options}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Component {...pageProps} />
     </AlertProvider>
   );
 }
