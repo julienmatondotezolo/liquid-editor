2712385d3dd559dd4950a73c0b2cf462420b43f3
Julien Matondo <julien.matondo-tezolo@bothive.be>
2022-02-22 10:41:17 +0100

    Adding layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <title>Bothive liquid editor</title>
+        <title>Bothive liquid editor | BETA</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>{children}</main>
