9ecfa0083b886626f0b214bbe8bfbe52d80d4f61
Julien Matondo <julien.matondo-tezolo@bothive.be>
2022-02-22 10:44:29 +0100

    adding layout to root

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,19 @@ import "../src/assets/styles/globals.scss";
 import { positions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
 
+import Layout from "../src/components/layout";
+
 const options = {
-  timeout: 3000,
+  timeout: 5000,
   position: positions.BOTTOM_CENTER,
 };
 
 function MyApp({ Component, pageProps }) {
   return (
     <AlertProvider template={AlertTemplate} {...options}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </AlertProvider>
   );
 }
