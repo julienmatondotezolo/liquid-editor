bd465c7f6e4efb941818b5c71e9a451bb9646a4f
Julien Matondo <julien.matondo-tezolo@bothive.be>
2022-03-08 16:08:39 +0100

    adding creatScenario function

--- src/components/scenarioPreviewPicker/index.js
+++ src/components/scenarioPreviewPicker/index.js
@@ -1,12 +1,20 @@
+import { useRouter } from "next/router";
 import React from "react";
-import { useRecoilValue } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
 
-import { scenariosAtom } from "../../recoil/atoms";
+import { scenariosAtom, selectedScenarioState } from "../../recoil/atoms";
 import ScenarioListPreviewPicker from "../scenarioListPreviewPicker";
 import styles from "./style.module.scss";
 
 export const ScenarioPreviewPicker = () => {
-  const scenarios = useRecoilValue(scenariosAtom);
+  const [scenarios, setScenarios] = useRecoilState(scenariosAtom);
+  const setSelectedScenario = useSetRecoilState(selectedScenarioState);
+  const router = useRouter();
+  const createNewScenario = () => {
+    setScenarios([...scenarios, scenarios.length]);
+    setSelectedScenario(scenarios.length);
+    router.push("/scenario");
+  };
 
   return (
     <section className={styles.extensionBox}>
@@ -17,6 +25,9 @@ export const ScenarioPreviewPicker = () => {
           {scenarios.map((id) => (
             <ScenarioListPreviewPicker id={id} key={id} />
           ))}
+          <button className={styles.btn} onClick={() => createNewScenario()}>
+            Add new scenario
+          </button>
         </div>
       </div>
     </section>
