772eb252cb538243bab51da559aec803a513772a
Julien Matondo <julien.matondo-tezolo@bothive.be>
2022-03-15 11:50:16 +0100

    adding scenario picker

--- src/components/scenarioListPreviewPicker/index.js
+++ src/components/scenarioListPreviewPicker/index.js
@@ -12,7 +12,9 @@ export const ScenarioListPreviewPicker = ({ id }) => {
 
   return (
     <p
-      className={selectedScenario === id ? styles.activeList : ""}
+      className={`${styles.scenarioListItems} ${
+        selectedScenario === id ? styles.activeList : ""
+      }`}
       onClick={() => setSelectedScenario(id)}
       aria-hidden="true"
     >
--- src/components/scenarioPreviewPicker/index.js
+++ src/components/scenarioPreviewPicker/index.js
@@ -1,23 +1,16 @@
-import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { useRecoilState, useSetRecoilState } from "recoil";
 
 import config from "../../config/config.json";
-import {
-  scenarioAtomFamily,
-  scenariosAtom,
-  selectedScenarioState,
-} from "../../recoil/atoms";
+import { scenariosAtom, selectedScenarioState } from "../../recoil/atoms";
 import ScenarioListPreviewPicker from "../scenarioListPreviewPicker";
 import styles from "./style.module.scss";
 
 export const ScenarioPreviewPicker = () => {
   const [scenarios, setScenarios] = useRecoilState(scenariosAtom);
-  const [selectedScenario, setSelectedScenario] = useRecoilState(
-    selectedScenarioState
-  );
-  const { name } = useRecoilValue(scenarioAtomFamily(selectedScenario));
+  const setSelectedScenario = useSetRecoilState(selectedScenarioState);
   const router = useRouter();
 
   const createNewScenario = () => {
@@ -27,26 +20,18 @@ export const ScenarioPreviewPicker = () => {
   };
 
   return (
-    <section className={styles.extensionBox}>
-      <p>
-        Preview:{" "}
-        <Link href={config.ROUTE.SCENARIO} passHref>
-          <a>
-            <strong>{name}</strong>
-          </a>
-        </Link>
-      </p>
-      <div className={styles.dropdown}>
-        <span>Pick scenario</span>
-        <div className={styles.dropdownContent}>
-          {scenarios.map((id) => (
-            <ScenarioListPreviewPicker id={id} key={id} />
-          ))}
-          <button className={styles.btn} onClick={createNewScenario}>
-            Add new scenario
-          </button>
-        </div>
+    <div className={styles.dropdown}>
+      <article className={styles.picker}>
+        <p>Pick scenario</p> <IoMdArrowDropdown />
+      </article>
+      <div className={styles.dropdownContent}>
+        {scenarios.map((id) => (
+          <ScenarioListPreviewPicker id={id} key={id} />
+        ))}
+        <button className={styles.btn} onClick={createNewScenario}>
+          Add new scenario
+        </button>
       </div>
-    </section>
+    </div>
   );
 };
