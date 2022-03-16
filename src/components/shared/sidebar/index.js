571dbb772d48522e3a7f9ae7cdac1e0f0490630b
Julien Matondo <julien.matondo-tezolo@bothive.be>
2022-03-16 13:00:31 +0100

    adding mouse action

--- src/components/shared/sidebar/index.js
+++ src/components/shared/sidebar/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoIosArrowBack } from "react-icons/io";
 import { useRecoilValue } from "recoil";
 
@@ -9,14 +9,25 @@ import styles from "./style.module.scss";
 
 const Sidebar = () => {
   const scenarios = useRecoilValue(scenariosAtom);
+  const [open, setOpen] = useState(false);
 
   return (
-    <nav className={styles.sidebar}>
-      <div>
+    <nav
+      className={styles.sidebar}
+      onMouseEnter={() => setOpen(!open)}
+      onMouseLeave={() => setOpen(!open)}
+      style={open ? { left: "0" } : { left: "-18%" }}
+    >
+      <div
+        className={styles.collapseIcon}
+        onClick={() => setOpen(!open)}
+        style={open ? { transform: "rotate(0deg)" } : { transform: "rotate(180deg)" }}
+        aria-hidden="true"
+      >
         <IoIosArrowBack />
       </div>
       <AddScenarioButton />
-      <ul>
+      <ul style={open ? { display: "block" } : { display: "none" }}>
         {scenarios.map((id) => (
           <ListScenario id={id} key={id} />
         ))}
