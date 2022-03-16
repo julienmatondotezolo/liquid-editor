abdec404253bb8f1b241334ebec721f8919c46c1
Julien Matondo <julien.matondo-tezolo@bothive.be>
2022-03-16 11:02:25 +0100

    changing navigation

--- src/components/shared/header/index.js
+++ src/components/shared/header/index.js
@@ -10,7 +10,7 @@ const Header = () => {
     <div>
       <nav className={styles.container}>
         <section className={styles.nav}>
-          <div
+          {/* <div
             className={styles.hamburger}
             onClick={() => setOpen(!open)}
             aria-hidden="true"
@@ -18,7 +18,7 @@ const Header = () => {
             <span className={styles.bar1}></span>
             <span className={styles.bar2}></span>
             <span className={styles.bar3}></span>
-          </div>
+          </div> */}
 
           <h1>Bothive Liquid editor</h1>
         </section>
