b1d30c8e2978d8af2c75ba4adb1bdd8cf23ff7b0
Niels Onderbeke <55786916+OnderbekeNiels@users.noreply.github.com>
2022-05-09 14:26:21 +0200

    rename vars

--- src/components/shared/header/index.js
+++ src/components/shared/header/index.js
@@ -1,11 +1,31 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
+import config from "../../../config/config.json";
 import styles from "./style.module.scss";
 
 export const Header = () => (
-  <nav className={styles.container}>
-    <section className={styles.nav}>
-      <h1>Bothive Liquid editor</h1>
-    </section>
-  </nav>
+  <header className={styles.container}>
+    <nav className={styles.nav}>
+      <h1>
+        Bothive Liquid editor
+        <Link href={config.BOTHIVE.SALES_PAGE}>
+          <a target="_blank">
+            <span className={styles.poweredByContent}>
+              powered by Bothive <Image src="/bothive_logo.svg" width={16} height={16}
+alt="Bothive logo"></Image>
+            </span>
+          </a>
+        </Link>
+      </h1>
+      <div className={styles.linkContainer}>
+        <Link href={config.BOTHIVE.SALES_PAGE}>
+          <a className={styles.link}>site</a>
+        </Link>
+        <Link href={config.BOTHIVE.APP}>
+          <a className={styles.link}>app</a>
+        </Link>
+      </div>
+    </nav>
+  </header>
 );
